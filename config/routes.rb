f16a6127f70db6d00d1d6c98e3f985a5502d574e
Sanjarito <santiago@nowhereprod.com>
2016-04-12 12:02:30 -0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace :apps do
     resources :texty, :only => [:index]
 
-  post 'apps/texty/send_text' => 'texty#send_text'
+  post '/texty/send_text' => 'texty#send_text'
   end
 
 
