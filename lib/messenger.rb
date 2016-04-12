b58ace874b306c570a8701018c59ec8987fd62c1
Sanjarito <santiago@nowhereprod.com>
2016-04-11 19:16:38 -0700

    all

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,8 +1,9 @@
 module Messenger
 
   def send_sms(number)
-    acct_sid = ENV['TWILIO_ACCT_SID']
-    auth_token = ENV['TWILIO_AUTH']
+    acct_sid = ENV["TWILIO_ACCT_SID"]
+    auth_token = ENV["TWILIO_AUTH"]
+
 
     @client = Twilio::REST::Client.new acct_sid, auth_token
 
@@ -11,7 +12,7 @@ module Messenger
     message = @client.account.messages.create(
         :from => from,
         :to => '+1'+number,
-        :body => 'Work Hard. Do Good. Change the World.'
+        :body => 'Work Hard. Do Good. Change the World. Ya hice una aplication que hace mensajes, mira metete a: https://lit-badlands-63534.herokuapp.com/apps/texty pon el telefono de mariana y dile que vamos a yoga'
         )
   end
 
