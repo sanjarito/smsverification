cecaff3f2fdef969d81a5c77d0dc884420c70070
Sanjarito <santiago@nowhereprod.com>
2016-04-12 11:41:53 -0700

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root '/texty/send_text'
     get '/about' => 'welcome#about'
 
   namespace :apps do
