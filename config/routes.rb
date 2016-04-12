825826d883f7e63c07df20d73540bc5007e36fbd
Sanjarito <santiago@nowhereprod.com>
2016-04-12 12:08:25 -0700

    all

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  
+  resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
-  root 'apps/texty#index'
-
+  root 'welcome#index'
+    get '/about' => 'welcome#about'
 
   namespace :apps do
     resources :texty, :only => [:index]
