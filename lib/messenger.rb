52dfe19cd19bdbed20bf6a8cd69533f56fc69e20
Sanjarito <santiago@nowhereprod.com>
2016-04-12 16:17:29 -0700

    all

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,15 +1,15 @@
 module Messenger
 
-  def send_sms(number,id)
+  def send_sms(number)
 
-
+    varx = 12345
 
     acct_sid = ENV["TWILIO_ACCT_SID"]
     auth_token = ENV["TWILIO_AUTH"]
 
 
 
-    @client = Twilio::REST::Client.new acct_sid, auth_token
+    @client = Twilio::REST::Client.new acct_sid, auth_token varx
 
     from = '+18446946384'
 
@@ -18,7 +18,7 @@ module Messenger
     message = @client.account.messages.create(
         :from => from,
         :to => '+1'+number,
-        :body =>  ' Your verification code is =  ' + number
+        :body =>  ' Your verification code is =  ' + number + varx
         )
   end
 
