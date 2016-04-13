40a453bc033aab99288d31da5c432b98a5a0bbf1
Sanjarito <santiago@nowhereprod.com>
2016-04-12 18:40:55 -0700

    all

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,27 +1,27 @@
-module Messenger
-
-  def send_sms(number)
-
-
-    @phone = Phone.find(params[:number])
-
-
-    acct_sid = ENV["TWILIO_ACCT_SID"]
-    auth_token = ENV["TWILIO_AUTH"]
-
-
-
-    @client = Twilio::REST::Client.new acct_sid, auth_token
-
-    from = '+18446946384'
-
-
-
-    message = @client.account.messages.create(
-        :from => from,
-        :to => '+1'+number,
-        :body =>  ' Your verification code is =  ' + number + "green"
-        )
-  end
-
-end
+# module Messenger
+#
+#   def send_sms(number)
+#
+#
+#
+#
+#
+#     acct_sid = ENV["TWILIO_ACCT_SID"]
+#     auth_token = ENV["TWILIO_AUTH"]
+#
+#
+#
+#     @client = Twilio::REST::Client.new acct_sid, auth_token
+#
+#     from = '+18446946384'
+#
+#
+#
+#     message = @client.account.messages.create(
+#         :from => from,
+#         :to => '+1'+number,
+#         :body =>  ' Your verification code is =  ' + number + "green"
+#         )
+#   end
+#
+# end
