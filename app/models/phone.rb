21bcdf0db4702203bdd11aee015de2d2a22825ab
Sanjarito <santiago@nowhereprod.com>
2016-04-12 17:34:00 -0700

    all

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -3,12 +3,14 @@ class Phone < ActiveRecord::Base
 
   include Messenger
 
-  
+
 
   def clean_number
     number = self.number.scan(/\d+/).join
     number[0] == "1" ? number[0] = '' : number
     number unless number.length != 10
+
+    id = self.id
   end
 
 
