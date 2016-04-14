fb6e57f22612a8bc39b1a5cf56ffbaa0be3b452f
Sanjarito <santiago@nowhereprod.com>
2016-04-13 17:06:17 -0700

    SDf

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014174043) do
+ActiveRecord::Schema.define(version: 20160413234930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "phones", force: :cascade do |t|
     t.string   "number"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
