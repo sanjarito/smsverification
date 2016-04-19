a4f2a43738a7c48b65f5ee28e46d266f85602e64
Sanjarito <santiago@nowhereprod.com>
2016-04-19 14:18:41 -0700

    asda

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -21,7 +21,7 @@ module Messenger
         :from => from,
         :to => '+1'+number,
 
-        :body =>  ' Welcome your verification token is =  ' + vercode + number
+        :body =>  ' Welcome =  ' + number + token
         )
     end
 end
