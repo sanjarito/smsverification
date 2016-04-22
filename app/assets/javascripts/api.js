f77227f2fdcc9bb3214220908270f54e141d1c4b
Sanjarito <santiago@nowhereprod.com>
2016-04-21 18:53:24 -0700

    Assad

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,10 +1,10 @@
-function submitForm(){
-  alert("we are inside button function");
-     $.ajax({
-         url: 'http://santiago.pixfizz.com/v1/admin/users/2760630',
-         method: 'POST',
-         data: YourData,
-         success: function(data){
-           console.log('succes: '+data);
-         }
-       });
+// function submitForm(){
+//   alert("we are inside button function");
+//      $.ajax({
+//          url: 'http://santiago.pixfizz.com/v1/admin/users/2760630',
+//          method: 'POST',
+//          data: YourData,
+//          success: function(data){
+//            console.log('succes: '+data);
+//          }
+//        });
