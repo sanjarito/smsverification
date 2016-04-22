537a0dac7b4d5ad536faa55afeca6ada3c8f22fa
Sanjarito <santiago@nowhereprod.com>
2016-04-21 19:32:35 -0700

    all

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -2,7 +2,7 @@ function submitForm(){
   alert("we are inside button function");
 
      $.ajax({
-         url: 'https://santiago.pixfizz.com/v1/admin/users/:2760630',
+         url: 'https://santiago.pixfizz.com/v1/admin/users/2760630',
          method: 'PUT',
          dataType: 'jsonp',
          username: 'santiago_casar@pixfizz.com',
