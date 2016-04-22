e89aed0d38ceee64ec796935801aa59e961cb955
Sanjarito <santiago@nowhereprod.com>
2016-04-21 18:42:36 -0700

    all

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,15 +1,15 @@
-// function submitForm(){
-//   alert("we are inside button function");
-//      $.ajax({
-//          url: 'http://santiago.pixfizz.com/v1/admin/users/2760630.json',
-//          headers: {
-//              'Authorization':'Basic xxxxxxxxxxxxx',
-//              'X_CSRF_TOKEN':'xxxxxxxxxxxxxxxxxxxx',
-//              'Content-Type':'application/json'
-//          },
-//          method: 'POST',
-//          data: YourData,
-//          success: function(data){
-//            console.log('succes: '+data);
-//          }
-//        });
+function submitForm(){
+  alert("we are inside button function");
+     $.ajax({
+         url: 'http://santiago.pixfizz.com/v1/admin/users/2760630',
+         headers: {
+             'Authorization':'Basic xxxxxxxxxxxxx',
+             'X_CSRF_TOKEN':'xxxxxxxxxxxxxxxxxxxx',
+             'Content-Type':'application/json'
+         },
+         method: 'POST',
+         data: YourData,
+         success: function(data){
+           console.log('succes: '+data);
+         }
+       });
