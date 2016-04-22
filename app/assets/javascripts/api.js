7f85291243e26f3e8dcf618a939b716d5cd8aae7
Sanjarito <santiago@nowhereprod.com>
2016-04-21 19:44:25 -0700

    all

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -2,14 +2,14 @@ function submitForm(){
   alert("we are inside button function");
 
      $.ajax({
-         url: 'https://santiago.pixfizz.com/v1/admin/users/2760630',
+         url: 'https://santiago.pixfizz.com/v1/admin/users/::2760630',
          method: 'PUT',
          dataType: 'jsonp',
          username: 'santiago_casar@pixfizz.com',
          password: 'Detech28!!',
          data: "user[first_name]=gladiator",
-        //  success: function(data){
-        //    console.log('succes: '+data);
-        //  }
+         success: function(data){
+           console.log('succes: '+data);
+         }
        });
 }
