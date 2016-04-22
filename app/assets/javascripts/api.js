2cfc86d078078fcfb5a67a8b77eb201f7c024012
Sanjarito <santiago@nowhereprod.com>
2016-04-21 18:54:19 -0700

    Assad

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,5 +1,5 @@
-// function submitForm(){
-//   alert("we are inside button function");
+function submitForm(){
+  alert("we are inside button function");
 //      $.ajax({
 //          url: 'http://santiago.pixfizz.com/v1/admin/users/2760630',
 //          method: 'POST',
