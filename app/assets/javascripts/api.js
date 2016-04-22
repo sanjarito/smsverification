20c20e5a2aea0392036d8366e317b5e61712df2f
Sanjarito <santiago@nowhereprod.com>
2016-04-21 18:32:19 -0700

    all

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,25 +1,16 @@
 function submitForm(){
   alert("we are inside button function");
-  $.ajax({
-     url: '/apps/texty/verify',
-     data: {
-        format: 'json'
-     },
-     dataType: 'json',
-     success: function(data) {
-        var $phone = $('<h1>').text(data.phones[0].phone_num);
-      },
-     type: 'GET'
-  });
- }
-
-
-  //
-  // $.ajax({
-  //   type: 'GET',
-  //   url: '/apps/texty/verify',
-  //   data: data,
-  //   success: success,
-  //   dataType: dataType
-  //   })
-  // }
+     $.ajax({
+         url: 'http://santiago.pixfizz.com/v1/admin/users/2760630.json',
+         headers: {
+             'Authorization':'Basic xxxxxxxxxxxxx',
+             'X_CSRF_TOKEN':'xxxxxxxxxxxxxxxxxxxx',
+             'Content-Type':'application/json'
+         },
+         method: 'POST',
+         dataType: 'json',
+         data: YourData,
+         success: function(data){
+           console.log('succes: '+data);
+         }
+       });
