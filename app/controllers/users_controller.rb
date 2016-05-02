ea8865e8c956694d00418ecea5e1404b077bb850
Sanjarito <santiago@nowhereprod.com>
2016-05-02 09:33:31 -0700

    Post request is working, passing last user id that registered and passing phone number and verification code back to it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
     USERNAME = "Api@hotmail.com" # needed to access the APi
     PASSWORD = "Api2000" # needed to access the APi
-    API_BASE_URL = "https://instantsignup.pixfizz.com/v1/admin/users" # base url of the API
+    API_BASE_URL = "http://instantsignup.pixfizz.com/v1/admin/users" # base url of the API
 
     def index
       @phone = Phone.last
@@ -21,17 +21,22 @@ class UsersController < ApplicationController
       if !User.exists?(@user.id)
       @user.save
 
+
      else
 
      end
 
-     RestClient::Resource.new(USERNAME,PASSWORD).put('http://instantsignup.pixfizz.com/v1/users/2760630', {:user => {:custom => {:telephone => "123123"}.to_json}}) do |response, request, result, &block|
-       if [301, 302, 307].include? response.code
-         redirected_url = response.headers[:location]
-       else
-         response.return!(request, result, &block)
-       end
-     end
+     user_api = RestClient::Resource.new('https://instantsignup.pixfizz.com', :user => USERNAME , :password => PASSWORD)
+    #  result = user_api["/v1/users/17659022.json"].put({:user =>{"first_name":"Floyd"}})
+    result = user_api["/v1/users/#{@user.id}.json"].put({:user =>{"custom" => {"telephone":"#{@phone.number}"}}})
+
+    #  RestClient::Resource.new(USERNAME,PASSWORD).put('http://instantsignup.pixfizz.com/v1/users/2760630', {:user => {:custom => {:telephone => "123123"}.to_json}}) do |response, request, result, &block|
+    #    if [301, 302, 307].include? response.code
+    #      redirected_url = response.headers[:location]
+    #    else
+    #      response.return!(request, result, &block)
+    #    end
+    #  end
 
       # posting = RestClient::Resource.new('https://instantsignup.pixfizz.com/v1/users', :user => USERNAME, :password => PASSWORD)
       # posting.post('/17527289', {:user => {:custom => {:telephone => "123123"}.to_json}})
@@ -42,40 +47,7 @@ class UsersController < ApplicationController
 end
 
 
-    #   RestClient::Resource.new(USERNAME,PASSWORD).post('http://instantsignup.pixfizz.com/v1/users/2760630', {:user => {:custom => {:telephone => "123123"}.to_json}})  do |response, request, result, &block|
-    #   if [301, 302, 307].include? response.code
-    #     redirected_url = response.headers[:location]
-    #   else
-    #     response.return!(request, result, &block)
-    #   end
-    # end
-# rest_resource_put.put "/2760630", {:user => {:custom => {:telephone => "123123"}.to_json}}
 
- #     RestClient::Request.execute(
- # method: :get,
- # url: 'http://instantsignup.pixfizz.com/v1/admin/users/',
- # user: USERNAME,
- # password: PASSWORD,
- # payload: { 'user[first_name]' => 'Juancho' }
-# )
-
-
-    # def update
-    #   @phone = Phone.last
-    #   @user = User.last
-    #     uri = "#{API_BASE_URL}/users/2760630" # specifying json format in the URl
-    #      RestClient.post 'http://instantsignup.pixfizz.com/v1/admin/users/2760630', {:user => {:custom => {:telephone => "123123"}.to_json}}
-    #    end
-    #
-
-    # def send
-    #   @phone = Phone.last
-    #   @user = User.last
-    #   uri = "#{API_BASE_URL}/users/2760630" # specifying json format in the URl
-    #   rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD)
-    #   rest_resource.post 'http://instantsignup.pixfizz.com/v1/admin/users/2760630', :nested => {:first_name => 'juancho'}.to_json
-    #
-    # end
 
     private
 
