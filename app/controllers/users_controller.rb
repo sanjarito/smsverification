58d8b0a2ad68e4ca9c9af12acad72c4bf4063993
Sanjarito <santiago@nowhereprod.com>
2016-05-04 15:14:17 -0700

    close to fixing from last user to current user framework

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,30 +9,30 @@ class UsersController < ApplicationController
     API_BASE_URL = "http://instantsignup.pixfizz.com/v1/admin/users" # base url of the API
 
     def index
-      @phone = Phone.last
-      @user = User.new
-      uri = "#{API_BASE_URL}.json" # specifying json format in the URl
-      uripost = "#{API_BASE_URL}"
-      rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD)
-      rest_resource_put = RestClient::Resource.new(uripost, USERNAME, PASSWORD)
-      users = rest_resource.get
-      @users = JSON.parse(users, :symbolize_names => true) # we will convert the return
-      @user.id = @users[-1][:id]
-      if !User.exists?(@user.id)
-      @user.save
+      # @phone = Phone.last
+      # @user = User.new
+      # uri = "#{API_BASE_URL}.json" # specifying json format in the URl
+      # uripost = "#{API_BASE_URL}"
+      # rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD)
+      # rest_resource_put = RestClient::Resource.new(uripost, USERNAME, PASSWORD)
+      # users = rest_resource.get
+      # @users = JSON.parse(users, :symbolize_names => true) # we will convert the return
+      # @user.id = @users[-1][:id]
+      # if !User.exists?(@user.id)
+      # @user.save
 
-
-     else
-
-     end
+     #
+    #  else
+     #
+    #  end
 
      user_api = RestClient::Resource.new('https://instantsignup.pixfizz.com', :user => USERNAME , :password => PASSWORD)
-    #  result = user_api["/v1/users/17659022.json"].put({:user =>{"first_name":"Floyd"}})
+     result = user_api["/v1/users/17659022.json"].put({:user =>{"first_name":"Floyd"}})
 
     # result = user_api["/v1/users/#{@user.id}.json"].put({:user =>{"custom" => {"telephone":"#{@phone.number}"}}})
     result = user_api["/v1/users/#{@user.id}.json"].put({:user =>{"custom" => {"vercode":"#{@phone.vercode}"}}})
 
-    redirect_to "http://instantsignup.pixfizz.com/site/nextsteps"
+
 
 
 
