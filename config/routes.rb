69ff255f40779630b11348ebfc06d8d56e2ed88d
Sanjarito <santiago@nowhereprod.com>
2016-05-05 14:24:04 -0700

    all

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'apps/texty#index'
+  put 'apps/texty' => 'texty#index'
 
   resources :users
   # get '/users' => 'users#index'
