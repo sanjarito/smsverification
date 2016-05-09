8db4f4a134877b7da9edc9392f8c4e5aa8d743c6
Sanjarito <santiago@nowhereprod.com>
2016-05-09 11:57:17 -0700

    asd

--- app/controllers/apps/texty_controller.rb
+++ app/controllers/apps/texty_controller.rb
@@ -29,8 +29,8 @@ class Apps::TextyController < ApplicationController
             rest_resource = RestClient::Resource.new(uri2, USERNAME, PASSWORD)
             user = rest_resource.get
             @user = JSON.parse(user, :symbolize_names => true) # we will convert the return
-          elsif User.exists?
-            @user = User.find(params[:id])
+          elsif User.find(params[:id]).exists?
+            
           end
             @phone.number = @user[:custom][:telephone]
 
