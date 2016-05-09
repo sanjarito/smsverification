a8a01465a077abd1f53580122fd68dcc64b00a54
Sanjarito <santiago@nowhereprod.com>
2016-05-09 13:34:25 -0700

    adding user belongs to phone model relation

---
+++ db/migrate/20160509202854_add_user_to_phone.rb
@@ -0,0 +1,5 @@
+class AddUserToPhone < ActiveRecord::Migration
+  def change
+    add_reference :phones, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422163059) do
+ActiveRecord::Schema.define(version: 20160509202854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,15 @@ ActiveRecord::Schema.define(version: 20160422163059) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "vercode"
+    t.integer  "user_id"
   end
+
+  add_index "phones", ["user_id"], name: "index_phones_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "phones", "users"
 end
