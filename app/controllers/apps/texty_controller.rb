637ba39b6ccde568accdcb3b41a44edd98f102b8
Sanjarito <santiago@nowhereprod.com>
2016-05-09 16:56:32 -0700

    Asda

--- app/controllers/apps/texty_controller.rb
+++ app/controllers/apps/texty_controller.rb
@@ -10,56 +10,53 @@ class Apps::TextyController < ApplicationController
   API_BASE_URL2 = "https://instantsignup.pixfizz.com/v1/users/" # base url of the API
 
   def index
-    # @phone = Phone.new
+   # @phone = Phone.new
 
-    @phone = Phone.new
-
-
-    # uri = "#{API_BASE_URL}.json?pages=10" # specifying json format in the URl
-    # rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD)
-    # users = rest_resource.get
-    # @users = JSON.parse(users, :symbolize_names => true) # we will convert the return
-
-            @user = User.new
-            if @user.id?
-              @user = User.last
-
-              redirect_to "/apps/texty/verify"
-            elsif !@user.id?
+   @phone = Phone.new
 
 
+   # uri = "#{API_BASE_URL}.json?pages=10" # specifying json format in the URl
+   # rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD)
+   # users = rest_resource.get
+   # @users = JSON.parse(users, :symbolize_names => true) # we will convert the return
+
+           @user = User.new
+           if @user.id?
+             @user = User.last
+
+             redirect_to "/apps/texty/verify"
+           elsif !@user.id?
+
+           @user.id = params[:user_id]
+           @user.save
+           uri2 = "#{API_BASE_URL2}#{@user.id}.json" # specifying json format in the URl
+           rest_resource = RestClient::Resource.new(uri2, USERNAME, PASSWORD)
+           user = rest_resource.get
+           @user = JSON.parse(user, :symbolize_names => true) # we will convert the return
+         end
+
+           session[:current_user_id] = @user.id
+           @phone.number = @user[:custom][:telephone]
+
+           if !Phone.exists?(@phone.vercode)
+           @phone.vercode = rand(10000..100000).to_s
+           @phone.send_sms(@phone.number,@phone.vercode)
+           @phone.save
+           redirect_to "/apps/texty/verify"
+
+           else
+           redirect_to "https://instantsignup.pixfizz.com/site"
+
+           end
+
+ end
+
+              
 
 
-            uri2 = "#{API_BASE_URL2}#{@user.id}.json" # specifying json format in the URl
-            rest_resource = RestClient::Resource.new(uri2, USERNAME, PASSWORD)
-            user = rest_resource.get
-            @user = JSON.parse(user, :symbolize_names => true) # we will convert the return
-            @user.id = params[:user_id]
-              @phone.user_id = @user.id
-              # session[:current_phone_id] = @phone.user_id
 
 
-              @user.save
-               session[:current_user_id] = @user.id
 
-
-          end
-
-
-            @phone.number = @user[:custom][:telephone]
-
-            if !Phone.exists?(@phone.vercode)
-            @phone.vercode = rand(10000..100000).to_s
-            @phone.send_sms(@phone.number,@phone.vercode)
-            @phone.save
-            redirect_to "/apps/texty/verify"
-
-            else
-            redirect_to "https://instantsignup.pixfizz.com/site"
-
-            end
-
-  end
 
   def show
      @user = User.find(params[:id])
@@ -126,7 +123,7 @@ end
 
 current_user = User.find_by_id(session[:current_user_id])
       @phone = Phone.find_by_id(current_user)
-      
+
 
 
   end
