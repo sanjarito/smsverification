dca6c92df76d3d57110ea9b3496d681aafbae5d4
Sanjarito <santiago@nowhereprod.com>
2016-05-13 11:05:50 -0700

    added lograge gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+
+  # custom 404
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActiveRecord::RecordNotFound,
+                ActionController::RoutingError,
+                ActionController::UnknownController,
+                ActionController::UnknownAction,
+                ActionController::MethodNotAllowed do |exception|
+
+      # Put loggers here, if desired.
+
+      redirect_to '/apps/texty/update_phone'
+    end
+  end
+
   protect_from_forgery with: :exception
 end
