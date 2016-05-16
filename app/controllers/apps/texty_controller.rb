b1dddae1055df6165baa365d0334d071d273bd2b
Sanjarito <santiago@nowhereprod.com>
2016-05-16 11:01:54 -0700

    update function not saving phone #

--- app/controllers/apps/texty_controller.rb
+++ app/controllers/apps/texty_controller.rb
@@ -74,6 +74,7 @@ class Apps::TextyController < ApplicationController
 
 def update_phone
     @phone = Phone.find_by_user_id(session[:user])
+    @user = User.find_by_id(session[:user])
     @phone.update(:number => params[:phone][:number])
     @phone.vercode = rand(10000..100000).to_s
     @phone.save
@@ -97,6 +98,7 @@ end
 
   def send_text
     @phone = Phone.find_by_user_id(session[:user])
+    @user = User.find_by_id(session[:user])
     @phone = Phone.create(phone_params)
     @phone.vercode = rand(10000..100000).to_s
     @phone.send_sms(@phone.number,@phone.vercode)
