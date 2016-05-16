9e47e8eb6a132f3a55a90a30848b88613834db41
Sanjarito <santiago@nowhereprod.com>
2016-05-16 11:24:41 -0700

    working last bugs with update vercode

--- app/controllers/apps/texty_controller.rb
+++ app/controllers/apps/texty_controller.rb
@@ -77,7 +77,7 @@ def update_phone
     @user = User.find_by_id(session[:user])
     @phone.update(:number => params[:phone][:number])
     @phone.vercode = rand(10000..100000).to_s
-    @phone.update(:vercode => phone.vercode)
+    @phone.update(:vercode => @phone.vercode)
     @phone.save
     @phone.send_sms(@phone.number,@phone.vercode)
 
