6ae6af763e7c1fce00b80f96122d1993418282f9
Sanjarito <santiago@nowhereprod.com>
2016-05-16 09:55:13 -0700

    All

---
+++ public/assets/api-8a4598e3b1f89b18d354ade5b6033493c32eda93081351072331051dd0aff1eb.js
@@ -0,0 +1,1 @@
+function submitForm(){alert("we are inside button function"),$.ajax({url:"https://santiago.pixfizz.com/v1/admin/users/2760630",method:"PUT",dataType:"jsonp",username:"santiago_casar@pixfizz.com",password:"Detech28!!",data:JSON.stringify({first_name:"Adam"}),success:function(s){console.log("succes: "+s)}})}
